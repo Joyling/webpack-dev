c364df46eabf5a9ea0f8e0ae38984a3739868ec1
chengaoling <chengaoling@sensetime.com>
2020-04-30 10:50:53 +0800

    chore: 打包前清理源目录文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 module.exports = {
   entry: './src/index.js', // 打包入口：指示 webpack 应该使用哪个模块，来作为构建其内部依赖图的开始
   output: {
@@ -88,6 +89,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].css',
       chunkFilename: '[id].css'
-    })
+    }),
+    new CleanWebpackPlugin()
   ] // 插件的配置：打包优化、资源管理和注入环境变量
 }
