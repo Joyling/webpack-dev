b237b43859539b1f1b76d5c0f6aa860e430fd8a6
chengaoling <chengaoling@sensetime.com>
2020-04-30 10:52:48 +0800

    chore: 提取公用代码

--- webpack.config.js
+++ webpack.config.js
@@ -91,5 +91,34 @@ module.exports = {
       chunkFilename: '[id].css'
     }),
     new CleanWebpackPlugin()
-  ] // 插件的配置：打包优化、资源管理和注入环境变量
+  ], // 插件的配置：打包优化、资源管理和注入环境变量
+  // 假如你 a.js 和 b.js 都 import 了 c.js 文件，这段代码就冗杂了。为什么要提取公共代码，简单来说，就是减少代码冗余，提高加载速度。
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          // 抽离自己写的公共代码
+          chunks: 'initial',
+          name: 'common', // 打包后的文件名，任意命名
+          minChunks: 2, //最小引用2次
+          minSize: 0 // 只要超出0字节就生成一个新包
+        },
+        styles: {
+          name: 'styles', // 抽离公用样式
+          test: /\.css$/,
+          chunks: 'all',
+          minChunks: 2,
+          enforce: true
+        },
+        vendor: {
+          // 抽离第三方插件
+          test: /node_modules/, // 指定是node_modules下的第三方包
+          chunks: 'initial',
+          name: 'vendor', // 打包后的文件名，任意命名
+          // 设置优先级，防止和自定义的公共代码提取时被覆盖，不进行打包
+          priority: 10
+        }
+      }
+    }
+  }
 }
